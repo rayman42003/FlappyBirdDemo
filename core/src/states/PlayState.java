3092ba9e7f0f96d55b041246f41d27d731196945
Richmond Chang <changrichmond@gmail.com>
2016-04-13 21:30:30 -0700

    Fixed a bug where the tube was respawning on the the damn bird.

--- core/src/states/PlayState.java
+++ core/src/states/PlayState.java
@@ -25,7 +25,7 @@ public class PlayState extends State {
 		bg = new Texture("bg.png");
 		
 		tubes = new Array<Tube>();
-		for(int i = 0; i < TUBE_COUNT; i++)
+		for(int i = 1; i <= TUBE_COUNT; i++)
 			tubes.add(new Tube(i *(TUBE_SPACING + Tube.TUBE_WIDTH)));
 	}
 
